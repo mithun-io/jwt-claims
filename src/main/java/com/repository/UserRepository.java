96f2ca675e4316ca8765a12774e18d7c706263e1
mithun <mithun-io@outlook.com>
2026-02-07 10:27:32 +0530

    Update UserRepository.java

--- src/main/java/com/repository/UserRepository.java
+++ src/main/java/com/repository/UserRepository.java
@@ -12,7 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUsername(String username);
 
-    Optional<User> findByUsername(String username);
-
     boolean existsByEmail(String email);
+
+    Optional<User> findByUsername(String username);
 }
